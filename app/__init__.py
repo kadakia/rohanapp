2f9702e38c7ce7b1b0a5575fad39beec96e7de4c
Rohan Kadakia <rmkadakia@gmail.com>
2018-04-23 19:44:48 -0400

    heroku deployment changes

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,7 @@ from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_babel import Babel
 import logging
-from logging.handlers import SMTPHandler, RotatingFileHandler
+from logging.handlers import SMTPHandler, RotatingFileHandler, StreamHandler
 import os
 from elasticsearch import Elasticsearch
 
@@ -62,12 +62,17 @@ def create_app(config_class=Config):
             mail_handler.setLevel(logging.ERROR) # only errors, not warnings
             app.logger.addHandler(mail_handler)
     
-        if not os.path.exists('logs'):
-            os.mkdir('logs')
-        file_handler = RotatingFileHandler('logs/rohanapp.log', maxBytes=10240, backupCount=10)
-        file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
-        file_handler.setLevel(logging.INFO)
-        app.logger.addHandler(file_handler)
+        if app.config['LOG_TO_STDOUT']:
+            stream_handler = logging.StreamHandler()
+            stream_handler.setLevel(logging.INFO)
+            app.logger.addHandler(stream_handler)
+        else:
+            if not os.path.exists('logs'):
+                os.mkdir('logs')
+            file_handler = RotatingFileHandler('logs/rohanapp.log', maxBytes=10240, backupCount=10)
+            file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+            file_handler.setLevel(logging.INFO)
+            app.logger.addHandler(file_handler)
 
         app.logger.setLevel(logging.INFO) # records each instance of the app starting up
         app.logger.info('RohanApp startup')
