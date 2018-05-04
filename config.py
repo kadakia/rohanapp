1b67339767c91c770ca8bab4c3573881bc180d67
Rohan Kadakia <rmkadakia@gmail.com>
2018-05-04 19:46:32 -0400

    changed redis to redistogo in config

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ class Config(object):
     MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
     ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
-    REDIS_URL = os.environ.get('REDIS_URL') or 'redis://' # REDISTOGO_URL
+    REDIS_URL = os.environ.get('REDISTOGO_URL') or 'redis://' # REDIS_URL
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25) # is int() really necessary here?
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None # TLS = Transport Layer Security
