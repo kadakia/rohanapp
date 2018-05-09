b0d216427cb21cd9ca4cf2e22836e1ef8db45e69
Rohan Kadakia <rmkadakia@gmail.com>
2018-05-09 16:09:52 -0400

    hardcoded redis url

--- config.py
+++ config.py
@@ -13,7 +13,8 @@ class Config(object):
     MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
     ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
-    REDIS_URL = os.environ.get('REDISTOGO_URL') or 'redis://' # REDIS_URL
+    REDIS_URL = 'redis://h:p23ccaf9db749cf631b6b462553407ae0e99286e3dbb9fec28283b60b88730ff5@ec2-52-55-90-143.compute-1.amazonaws.com:45769'
+    # os.environ.get('REDISTOGO_URL') or 'redis://' # REDIS_URL
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25) # is int() really necessary here?
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None # TLS = Transport Layer Security
