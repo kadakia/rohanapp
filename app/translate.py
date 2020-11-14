f7a0d6d79ba5c48fd8fe03534340cdc590004eac
Rohan Kadakia <rmkadakia@gmail.com>
2020-11-14 11:52:27 -0800

    Update language translation from v2 to v3

--- app/translate.py
+++ app/translate.py
@@ -1,14 +1,31 @@
 from flask import current_app
 import json
 import requests
-# from flask_babel import _
+
 
 def translate(text, source_language, dest_language):
-    if 'MS_TRANSLATOR_KEY' not in current_app.config or not current_app.config['MS_TRANSLATOR_KEY']: # what's the difference ?!
+    """
+    """
+    if 'MS_TRANSLATOR_KEY' not in current_app.config or not current_app.config['MS_TRANSLATOR_KEY']:
         return 'Error: the translation service is not configured.'
-    auth = {'Ocp-Apim-Subscription-Key': current_app.config['MS_TRANSLATOR_KEY']} # what is Ocp ?
-    r = requests.get('https://api.microsofttranslator.com/v2/Ajax.svc/Translate?text={}&from={}&to={}'.format(
-        text, source_language, dest_language), headers=auth)
+
+    auth = {
+        'Ocp-Apim-Subscription-Key': current_app.config['MS_TRANSLATOR_KEY']}
+
+    r = requests.post(
+        'https://api.cognitive.microsofttranslator.com/translate?api-version=3.0&from={}&to={}'.format(
+            source_language, dest_language
+        ), headers=auth, json={'Text': text}
+    )
+
+    # r = requests.get(
+    #     'https://api.microsofttranslator.com/v2/Ajax.svc/Translate?text={}&from={}&to={}'.format(
+    #         text, source_language, dest_language
+    #     ), headers=auth
+    # )
+
     if r.status_code != 200:
         return 'Error: the translation service failed.'
-    return json.loads(r.content.decode('utf-8-sig')) # utf-8-sig is Microsoft variant of utf-8
+
+    # utf-8-sig is Microsoft variant of utf-8
+    return json.loads(r.content.decode('utf-8-sig'))
