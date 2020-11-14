b333727ee89614104746c2c45224c657f68e3a4c
Rohan Kadakia <rmkadakia@gmail.com>
2020-11-14 15:43:43 -0800

    Clean up the config file

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 import os
-from dotenv import load_dotenv # Do not add .env file to version control !!! ***
+from dotenv import load_dotenv  # Do not add .env file to version control !!! ***
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
+
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
@@ -13,12 +14,10 @@ class Config(object):
     MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
     ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
-    REDIS_URL = os.environ.get('REDIS_URL') or 'redis://' # REDISTOGO_URL, REDISCLOUD
+    REDIS_URL = os.environ.get('REDIS_URL') or 'redis://'  # REDISTOGO_URL, REDISCLOUD
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25) # is int() really necessary here?
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None # TLS = Transport Layer Security
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)  # Is int() really necessary here?
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['rohanapp.email@gmail.com']
-
-# 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
